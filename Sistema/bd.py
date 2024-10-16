f14a91ff04d5c9d7ca8d74b989a40e44f94a2cce
Jegis9 <ixcaquic22@gmailcom>
2024-10-16 10:56:54 -0600

    Descripción de los cambios

---
+++ Sistema/bd.py
@@ -0,0 +1,10 @@
+MYSQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
+        # 'HOST': 'backend-pg.c32004mmq5g8.us-east-2.rds.amazonaws.com',
+        'PORT': '3307',
+    }
+}
