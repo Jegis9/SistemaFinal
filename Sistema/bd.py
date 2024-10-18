e7847e310b21b43f06a825b92b1b3ef5aa6c5c6f
Jegis9 <ixcaquic22@gmailcom>
2024-10-18 11:49:05 -0600

    limpio

--- Sistema/bd.py
+++ Sistema/bd.py
@@ -12,15 +12,3 @@ MYSQL = {
 }
 
 
-# MYSQL = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'estaciondatabasebmt',
-#         'USER': 'aestacionbd',
-#         'PASSWORD': 'baseD3d4t0sE27ac10n',
-#         'HOST': 'databaseestacion.c32004mmq5g8.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#     }
-# }
-
-
