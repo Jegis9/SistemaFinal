f8bdcd3b81bf5412b86e680fb7a1a2663843160d
Jegis9 <ixcaquic22@gmailcom>
2024-10-30 11:43:44 -0600

    prueba agregar eliminar usuario,

--- Aplicaciones/lUsuarios/views.py
+++ Aplicaciones/lUsuarios/views.py
@@ -44,17 +44,20 @@ def lInternos(request):
 def desactivar_usuario(request, user_id):
     user = get_object_or_404(User, id=user_id)
     if user.is_active:
-        # Desactivar usuario
         user.is_active = False
         user.save()
         messages.success(request, f'Usuario {user.username} desactivado exitosamente.')
-    
-    # Después de desactivar, eliminar el usuario si ya está inactivo
-    if not user.is_active:
-        user.delete()
-        messages.success(request, f'Usuario {user.username} eliminado exitosamente.')
-    
+    else:
+        messages.warning(request, f'El usuario {user.username} ya está desactivado.')
     return redirect('lInternos')  # Reemplaza con el nombre de tu URL
+
+
+def eliminar_persona(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user.delete()
+    messages.success(request, f'Usuario {user.username} eliminado exitosamente.')
+    return redirect('lInternos')  # Reemplaza con el nombre de tu URL
+
 
 @login_required
 @user_passes_test(is_admin, login_url='error')
